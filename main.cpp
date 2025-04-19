519041cb4205a31060fc9fa2e9a3b091684fbb7a
Max B. <77401015+maxboro@users.noreply.github.com>
2025-04-19 11:55:47 +0300

    Refactoring (#3)
    * Move saved video params to separate function
    
    * Move writers init to separate function
    
    * Add separate functions for writing to file
    
    * Remove redundant frame_final variable, use references instead of pointers in process_frame
    
    * Remove const from process_frame
    
    * Make bbox const in person_alike, center_of_bbox
    
    * Add flags
    
    * Use constexpr instead of macroses
    
    * Use references instead of pointers in initialize_writers
    
    * Make an only function for writing to video file
    
    * Simplify person_alike
    
    * Remove coping frame in write_to_file
    
    * Replace pointers with reference in get_params()
    
    * Make references const in get_params(), write_to_file()
    
    * Make input video path a constexpr
    
    * Add directory creation

--- main.cpp
+++ main.cpp
@@ -1,34 +1,34 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <filesystem>
 
-#define VIDEO_SAVE_RESIZE_COEF 0.5
-#define MIN_AREA_PIX 100
-#define MAX_AREA_PIX 3000
+constexpr double VIDEO_SAVE_RESIZE_COEF = 0.5;
+constexpr int MIN_AREA_PIX = 100;
+constexpr int MAX_AREA_PIX = 3000;
+constexpr const char* input_video_path = "./data/actions2.mpg";
 
-bool person_alike(cv::Rect bbox){
-    bool shaped_as_person = bbox.height > bbox.width;
-    float area = bbox.height * bbox.width;
-    bool is_small = area > MIN_AREA_PIX && area < MAX_AREA_PIX;
-    if (shaped_as_person && is_small){
-        return true;
-    } else {
-        return false;
-    }
+struct SavedVideoParams {
+    int frame_width;
+    int frame_height;
+    double fps;
+};
+
+bool person_alike(const cv::Rect& bbox){
+    const bool shaped_as_person = bbox.height > bbox.width;
+    const int area = static_cast<int>(bbox.area());
+    const bool is_small = area > MIN_AREA_PIX && area < MAX_AREA_PIX;
+    return shaped_as_person && is_small;
 }
 
-cv::Point center_of_bbox(cv::Rect bbox){
+cv::Point center_of_bbox(const cv::Rect& bbox){
     int x, y;
     x = bbox.x + (bbox.width / 2);
     y = bbox.y + (bbox.height / 2);
     return cv::Point(x, y);
 }
 
-void process_frame(cv::Mat frame, cv::Mat* frame_proc_ptr, cv::Mat* frame_final_ptr){
-    cv::Mat frame_proc, frame_final;
-
-    frame_final = frame.clone();
-
+void process_frame(cv::Mat& frame, cv::Mat& frame_proc){
     // to back and white
     cv::cvtColor(frame, frame_proc, cv::COLOR_BGR2GRAY);
 
@@ -53,51 +53,86 @@ void process_frame(cv::Mat frame, cv::Mat* frame_proc_ptr, cv::Mat* frame_final_
         if (person_alike(bbox)){
             // draw ROI
             cv::rectangle(frame_proc, bbox, cv::Scalar(125, 125, 125), 4); 
-            cv::circle(frame_final,	center_of_bbox(bbox), 1, cv::Scalar(0, 0, 255), 5);
+            cv::circle(frame, center_of_bbox(bbox), 1, cv::Scalar(0, 0, 255), 5);
         }
     }
+}
 
-    *frame_proc_ptr = frame_proc;
-    *frame_final_ptr = frame_final;
+struct SavedVideoParams get_params(const cv::VideoCapture& cap){ 
+    struct SavedVideoParams params;
+    params.frame_width = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH) * VIDEO_SAVE_RESIZE_COEF);
+    params.frame_height = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT) * VIDEO_SAVE_RESIZE_COEF);
+    params.fps = cap.get(cv::CAP_PROP_FPS);
+    return params;
+}
+
+bool initialize_writers(const struct SavedVideoParams& video_params, cv::VideoWriter& writer, cv::VideoWriter& writer_proc){
+
+    // writer for original video with marks
+    writer.open(
+        "./output/output.mp4", 
+        cv::VideoWriter::fourcc('m', 'p', '4', 'v'), // MP4 codec
+        video_params.fps, 
+        cv::Size(video_params.frame_width, video_params.frame_height)
+    );
+
+    if (!writer.isOpened()) {
+        std::cerr << "Error: Cannot open the video writer.\n" << std::endl;
+        return false;
+    }
+
+
+    // writer for processed video
+    writer_proc.open(
+        "./output/output_proc.mp4", 
+        cv::VideoWriter::fourcc('m', 'p', '4', 'v'), // MP4 codec
+        video_params.fps, 
+        cv::Size(video_params.frame_width, video_params.frame_height)
+    );
+
+
+    if (!writer_proc.isOpened()) {
+        std::cerr << "Error: Cannot open the video writer.\n" << std::endl;
+        return false;
+    }
+
+    // if success return true
+    return true;
+}
+
+void write_to_file(const cv::Mat& frame, const struct SavedVideoParams& video_params, cv::VideoWriter& writer, bool is_grey){
+    cv::Mat frame_tmp;
+    cv::resize(frame, frame_tmp, cv::Size(video_params.frame_width, video_params.frame_height));
+    if (is_grey){
+        cv::cvtColor(frame_tmp, frame_tmp, cv::COLOR_GRAY2BGR);
+    }
+    writer.write(frame_tmp);
 }
 
 int main() {
-    std::string videoPath = "./data/actions2.mpg";
+    std::filesystem::create_directory("./output");
 
     // Open the video file
-    cv::VideoCapture cap(videoPath);
+    cv::VideoCapture cap(input_video_path);
     if (!cap.isOpened()) {
         std::cerr << "Error: Cannot open the video file." << std::endl;
         return -1;
     }
 
     // for saving video
-    int frame_width = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH) * VIDEO_SAVE_RESIZE_COEF);
-    int frame_height = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT) * VIDEO_SAVE_RESIZE_COEF);
-    double fps = cap.get(cv::CAP_PROP_FPS);
-    cv::VideoWriter writer(
-        "./output/output.mp4", 
-        cv::VideoWriter::fourcc('m', 'p', '4', 'v'), // MP4 codec
-        fps, 
-        cv::Size(frame_width, frame_height)
-    );
-    cv::VideoWriter writer_proc(
-        "./output/output_proc.mp4", 
-        cv::VideoWriter::fourcc('m', 'p', '4', 'v'), // MP4 codec
-        fps, 
-        cv::Size(frame_width, frame_height)
-    );
-
-    if (!writer.isOpened()) {
-        std::cerr << "Error: Cannot open the video writer.\n" << std::endl;
+    struct SavedVideoParams video_params = get_params(cap);
+    cv::VideoWriter writer, writer_proc;
+    bool writer_init_is_successful = initialize_writers(video_params, writer, writer_proc);
+    if (!writer_init_is_successful){
+        std::cerr << "Error: Writers init wasn't successful." << std::endl;
         return -1;
     }
-
+    
     // Window to show the video
     cv::namedWindow("Video with detection", cv::WINDOW_AUTOSIZE);
     cv::namedWindow("Video processed", cv::WINDOW_AUTOSIZE);
 
-    cv::Mat frame, frame_proc, frame_final;
+    cv::Mat frame, frame_proc;
     while (true) {
         // Read next frame
         cap >> frame;
@@ -106,18 +141,14 @@ int main() {
         if (frame.empty())
             break;
 
-        process_frame(frame, &frame_proc, &frame_final);
+        process_frame(frame, frame_proc);
 
         // Display the frame
-        cv::imshow("Video with detection", frame_final);
+        cv::imshow("Video with detection", frame);
         cv::imshow("Video processed", frame_proc);
 
-        cv::resize(frame_final, frame_final, cv::Size(frame_width, frame_height));
-        writer.write(frame_final);
-
-        cv::resize(frame_proc, frame_proc, cv::Size(frame_width, frame_height));
-        cv::cvtColor(frame_proc, frame_proc, cv::COLOR_GRAY2BGR);
-        writer_proc.write(frame_proc);
+        write_to_file(frame, video_params, writer, false);
+        write_to_file(frame_proc, video_params, writer_proc, true);
 
         // Wait for 30ms and break if 'q' is pressed
         if (cv::waitKey(30) == 'q')
@@ -127,6 +158,7 @@ int main() {
     // Release resources
     cap.release();
     writer.release();
+    writer_proc.release();
     cv::destroyAllWindows();
     return 0;
 }
